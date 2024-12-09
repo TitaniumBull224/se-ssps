e97249b563363b47c3a68b11ca5f28998be2d471
Sơn Bùi Tiến Trường <titaniumyama224@gmail.com>
2024-12-09 22:31:53 +0700

    minor fix

--- ssps/js/login.js
+++ ssps/js/login.js
@@ -1,8 +1,15 @@
-const switchers = [...document.querySelectorAll('.switcher')]
+const switchers = [...document.querySelectorAll(".switcher")];
 
-switchers.forEach(item => {
-	item.addEventListener('click', function() {
-		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
-		this.parentElement.classList.add('is-active')
-	})
-})
+switchers.forEach((item) => {
+	item.addEventListener("click", function () {
+		switchers.forEach((item) =>
+			item.parentElement.classList.remove("is-active"),
+		);
+		this.parentElement.classList.add("is-active");
+	});
+});
+
+function Login() {
+	alert("Login Successful");
+	window.location.href = "dashboard.html";
+}
